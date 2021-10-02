9dd8a9ec4cbed2f6864340e3e075878038f72c59
ogaamyr <aamir@ogaamyr.com>
2021-10-03 00:21:13 +0400

    setting dynamic rounte and getting dynamic request value/id

---
+++ pages/news/[newsId].js
@@ -0,0 +1,21 @@
+// served when you try to reach domain.com/news/some-path-name
+
+import { useRouter } from "next/router";
+
+function DetailPage() {
+  const router = useRouter();
+
+  const newsId = router.query.newsId;
+
+  // use the dynamic value to get some unique data
+  // maybe get news info from the database
+
+  return (
+    <div>
+      <h1>News Item Page</h1>
+      <p>Requested ID: {newsId}</p>
+    </div>
+  );
+}
+
+export default DetailPage;
